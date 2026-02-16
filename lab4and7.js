73f0f60bc8a9923d7393150073260dda730339c6
Kim Lengen Gesite <kimlengel0408@gmail.com>
2026-02-16 19:58:04 +1100

    New draft

---
+++ lab4and7.js
@@ -0,0 +1,32 @@
+let a = 10;
+let b = 10;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+
+}
+
+function nameCall(name){
+    return "Hello" + " " + name;
+
+}
+
+alert(add(a,b));
+alert(subtract(a,b));
+alert(multiply(a,b));
+alert(divide(a,b));
+alert(nameCall("Kim"))
